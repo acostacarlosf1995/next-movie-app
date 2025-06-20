2c1865985a3701d4202a9f23923e6934c6c17132
Carlos Acosta <carlosacostaf1995@gmail.com>
2025-06-20 14:08:57 -0600

    refactoring

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -1,29 +1,29 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
-import { Navbar } from "../ui";
+import {Navbar} from "../ui";
 
 const Scene = dynamic(() => import('../three/Scene').then((mod) => mod.Scene), {
     ssr: false,
 });
 
-export const Layout = ({ children, title }) => {
+export const Layout = ({children, title}) => {
     return (
         <>
             <Head>
-                <title>{ title || "Movies App" }</title>
-                <meta name="author" content="Carlos Acosta" />
-                <meta name="description" content={`Informacion sobre la pelicula ${ title }`} />
-                <meta name="keywords" content={`${ title }, movies, ${ title || "genre" }`} />
+                <title>{title || "Movies App"}</title>
+                <meta name="author" content="Carlos Acosta"/>
+                <meta name="description" content={`Informacion sobre la pelicula ${title}`}/>
+                <meta name="keywords" content={`${title}, movies, ${title || "genre"}`}/>
             </Head>
 
-            <Scene />
+            <Scene/>
 
-            <div style={{ position: 'relative', zIndex: 1 }}>
-                <Navbar />
+            <div style={{position: 'relative', zIndex: 1}}>
+                <Navbar/>
                 <main style={{
                     padding: '0px 20px'
                 }}>
-                    { children }
+                    {children}
                 </main>
             </div>
         </>

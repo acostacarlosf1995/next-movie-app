99e83dbf6c6663322bada0209a77fdb456eda69e
Carlos Acosta <carlosacostaf1995@gmail.com>
2025-06-20 19:48:21 -0600

    feat: Implement 3D cards, carousel, and fix multiple bugs

--- components/three/Scene.jsx
+++ components/three/Scene.jsx
@@ -1,12 +1,26 @@
-import {Canvas} from "@react-three/fiber";
-import {Stars} from "./Stars";
+import { useRef } from 'react';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Stars } from "@react-three/drei";
+
+const AnimatedStars = () => {
+  const starsRef = useRef();
+
+  useFrame((state, delta) => {
+    if (starsRef.current) {
+      starsRef.current.rotation.x -= delta / 10;
+      starsRef.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return <Stars ref={starsRef} radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />;
+};
 
 export const Scene = () => {
     return (
         <div style={{position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0}}>
             <Canvas camera={{position: [0, 0, 1]}}>
                 <color attach="background" args={['#000']}/>
-                <Stars/>
+                <AnimatedStars />
             </Canvas>
         </div>
     )
--- pages/search/[id].jsx
+++ pages/search/[id].jsx
@@ -1,29 +1,23 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import {Text} from "@nextui-org/react";
+import {Text, Grid, Pagination} from "@nextui-org/react";
 
 import {Layout} from "../../components/layouts";
 import {searchMoviesApi} from "../../api";
-import {MoviesCard} from "../../components/movies";
-
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Virtual } from 'swiper/modules';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+import { MoviesCard } from '../../components/movies';
 
 const API_KEY = "dfa1345af4b42814b7229dbfa7ab4cfc"
 
-const ResultsPage = ({ searchResults }) => {
+const ResultsPage = ({ searchResults, totalPages, currentPage, searchTerm }) => {
 
     const router = useRouter()
 
-    const titleResult = router.query.id.replaceAll('-', ' ')
+    const onPageChange = (page) => {
+        router.push(`/search/${searchTerm}?page=${page}`)
+    }
 
     return (
-        <Layout title={`Search ${titleResult}`}>
+        <Layout title={`Search ${searchTerm}`}>
             <div
                 style={{
                     display: "flex",
@@ -31,65 +25,51 @@ const ResultsPage = ({ searchResults }) => {
                     marginTop: "10px",
                     marginBottom: "10px"
                 }}>
-                <Text color='white' h3 >{`Results for "${titleResult}"`}</Text>
+                <Text color='white' h3 >{`Results for "${searchTerm}"`}</Text>
             </div>
-            <Swiper
-              modules={[Navigation, Virtual]}
-              virtual
-              navigation
-              spaceBetween={10}
-              breakpoints={{
-                // when window width is >= 640px
-                640: {
-                  slidesPerView: 2,
-                  spaceBetween: 15,
-                },
-                // when window width is >= 768px
-                768: {
-                  slidesPerView: 3,
-                  spaceBetween: 20,
-                },
-                // when window width is >= 1024px
-                1024: {
-                  slidesPerView: 4,
-                  spaceBetween: 25,
-                },
-              }}
-              style={{
-                width: '100%',
-                height: '100%',
-              }}
-            >
-                {searchResults.map( (movies, index) =>
-                    <SwiperSlide key={movies.id} virtualIndex={index}>
-                      <MoviesCard
-                          movies={movies}
-                      />
-                    </SwiperSlide>
-                )}
-            </Swiper>
+            <Grid.Container gap={2} justify="center">
+              {searchResults.slice(0, 12).map((movie) => (
+                <Grid xs={8} sm={6} md={4} lg={2} key={movie.id}>
+                  <MoviesCard movies={movie} />
+                </Grid>
+              ))}
+            </Grid.Container>
+
+            { totalPages > 1 && (
+                <div style={{ padding: '2rem 0', display: 'flex', justifyContent: 'center' }}>
+                    <Pagination
+                        total={totalPages}
+                        initialPage={currentPage}
+                        onChange={onPageChange}
+                        color="primary"
+                    />
+                </div>
+            )}
         </Layout>
     )
 }
 
 export async function getServerSideProps(ctx) {
 
-    const searchTerm = ctx.query.id
+    const { id: searchTerm, page = 1 } = ctx.query;
 
-    const { data } = await searchMoviesApi.get(`/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=1&include_adult=false`)
+    const { data } = await searchMoviesApi.get(`/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=${page}&include_adult=false`);
 
     const searchResults = data.results.map( movie => ({
         title: movie.title || 'No Title',
         id: movie.id,
         poster: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : '/images/poster-holder.jpg',
         rate: movie.vote_average
-    }))
+    }));
 
     return {
         props: {
-            searchResults
+            searchResults,
+            totalPages: data.total_pages > 500 ? 500 : data.total_pages,
+            currentPage: Number(page),
+            searchTerm
         }
-    }
+    };
 }
 
 export default ResultsPage;
